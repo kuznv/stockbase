9ec633484a62a11799ed1f32e693ee93d2e78773
c57m7a <pa7z2kd@yandex.ru>
2020-01-19 19:21:04 +0300

    Remove unused configuration

--- src/main/java/stockbase/app/DefaultServiceConfiguration.java
+++
@@ -1,9 +0,0 @@
-package stockbase.app;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties("service")
-public class DefaultServiceConfiguration {
-    private String authAddress = "https://stg-gateway.stockbase.io/oauth/token";
-    private String transactionAddress = "https://stg-gateway.stockbase.io/api/payment/create/oauth/token";
-}
