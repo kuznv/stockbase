0edbd560bd96de8aacf20b6562ad7d31e9aacdb7
c57m7a <pa7z2kd@yandex.ru>
2020-01-19 18:32:28 +0300

    Tests

--- src/test/java/stockbase/app/ApplicationTests.java
+++ src/test/java/stockbase/app/ApplicationTests.java
@@ -1,13 +1,71 @@
 package stockbase.app;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import stockbase.impl.api.authorization.AuthorizationRequest;
+import stockbase.impl.api.notification.NotificationRequest;
+import stockbase.impl.api.notification.NotificationResponse;
+import stockbase.impl.api.transaction.CreateTransactionRequest;
+import stockbase.impl.authentification.CAuthenticationData;
+import stockbase.interfaces.api.authorization.IAuthorizationResponse;
+import stockbase.interfaces.api.notification.NotificationException;
+import stockbase.interfaces.authentification.ICAuthenticationData;
+import stockbase.interfaces.service.IPaymentService;
+
+import java.util.Random;
 
 @SpringBootTest
 class ApplicationTests {
+    @Value("${test.clientId}")
+    private String clientId;
 
-	@Test
-	void contextLoads() {
-	}
+    @Value("${test.clientSecret}")
+    private String clientSecret;
+
+    private IPaymentService paymentService;
+    private ICAuthenticationData authenticationData;
+    private String transactionId;
+
+
+    @Autowired public ApplicationTests(IPaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @Test
+    @BeforeEach
+    void authorizationTest() {
+        AuthorizationRequest request = new AuthorizationRequest(clientId, clientSecret);
+        IAuthorizationResponse authorizationResponse = paymentService.authorize(request);
+        String accessToken = authorizationResponse.getAccessToken();
+        Assertions.assertNotNull(accessToken);
+        authenticationData = new CAuthenticationData(accessToken);
+    }
+
+    @Order(1)
+    @Test
+    void transactionTest() {
+        transactionId = String.valueOf(new Random().nextInt());
+        CreateTransactionRequest createTransaction = new CreateTransactionRequest("eth", 20.0, transactionId);
+        paymentService.createTransaction(createTransaction, authenticationData);
+    }
+
+    @Order(2)
+    @Test
+    void notificationTest() throws NotificationException {
+        NotificationRequest notificationRequest = new NotificationRequest(
+                "abc",
+                transactionId,
+                "success",
+                "123",
+                "eth"
+        );
+        NotificationResponse notificationResponse = paymentService.onNotificationRequest(notificationRequest);
+        Assertions.assertEquals("success", notificationResponse.getResponse());
+    }
 
 }
